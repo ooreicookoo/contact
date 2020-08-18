b18e84aa04e38cd8cbfc684b7446bbe065653e40
Reiko Enomoto <ooreicookoo@gmail.com>
2020-08-18 13:37:55 +0900

    完成

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,23 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
+    # binding.pry
   end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+  def contact_params
+    params.require(:contact).permit(:name, :email, :content)
+  end
+
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,3 @@
 class Contact < ApplicationRecord
+  validates :content, length: {in: 1..140}
 end
